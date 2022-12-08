a63d6414bdf2536618e7115e08b30ea754136340
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-08 17:28:41 +0100

    iteration 5

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,32 @@
 
   <p>Iteration 4</p>
   <Posts></Posts>
+
+  <p>Iteration 5</p>
+  <div :class="{ blue: changeColor }">I am learning Vue</div>
+  <button @click="changeColor = !changeColor">Toggle</button>
+
   <p>Iteration 3</p>
   <Footer></Footer>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 import Posts from "./components/Posts.vue";
 function operation(a, b) {
   return a + b;
 }
+
+let changeColor = ref(true);
 </script>
 
-<style></style>
+<style>
+.blue {
+  background-color: blue;
+  color: white;
+  width: 30rem;
+  border: solid 1px black;
+}
+</style>
