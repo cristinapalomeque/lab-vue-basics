ec29592e92991d63dcaaa65b83fc0e3e84ee0149
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-08 12:00:15 +0100

    iteration 3 toggle button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,19 @@
     <a>About us</a>
     <a>Social Media</a>
   </div>
+  <button @click="showFooter = !showFooter">Toggle</button>
 </template>
 
-<script setup>
-import { ref } from "vue";
-let showFooter = ref(true);
+<script>
+// import { ref } from "vue";
+// let showFooter = ref(true);
+export default {
+  data() {
+    return {
+      showFooter: true,
+    };
+  },
+};
 </script>
 
 <style></style>
